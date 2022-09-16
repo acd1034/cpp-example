bebfb045897451f467d05e09fb1ba55dc54f1d88
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-16 13:51:33 +0900

    Init

--- tests/main/main.cpp
+++ tests/main/main.cpp
@@ -1,7 +1,3 @@
 #include <catch2/catch_test_macros.hpp>
-#include <iris/iris.hpp>
 
-TEST_CASE("main", "[main][squared]") {
-  static_assert(std::is_same_v<decltype(iris::squared(0)), int>);
-  CHECK(iris::squared(2) == 4);
-}
+// TEST_CASE("main", "[main]") {}
