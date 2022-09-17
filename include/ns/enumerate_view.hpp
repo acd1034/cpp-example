39e3707e2274f61afbe6fa831ad9541f428a8793
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-17 14:49:14 +0900

    一行80文字に変更

--- include/ns/enumerate_view.hpp
+++ include/ns/enumerate_view.hpp
@@ -36,8 +36,12 @@ namespace ns {
     constexpr iterator(std::ranges::iterator_t<View> current, std::size_t count)
       : current_(std::move(current)), count_(std::move(count)) {}
 
-    constexpr const std::ranges::iterator_t<View>& base() const& noexcept { return current_; }
-    constexpr std::ranges::iterator_t<View> base() && { return std::move(current_); }
+    constexpr const std::ranges::iterator_t<View>& base() const& noexcept {
+      return current_;
+    }
+    constexpr std::ranges::iterator_t<View> base() && {
+      return std::move(current_);
+    }
 
     constexpr std::pair<std::size_t, std::ranges::range_reference_t<View>> //
     operator*() const {
@@ -60,12 +64,16 @@ namespace ns {
     std::ranges::sentinel_t<View> end_ = std::ranges::sentinel_t<View>();
 
   public:
-    sentinel() requires std::default_initializable<std::ranges::sentinel_t<View>>
+    sentinel() requires
+      std::default_initializable<std::ranges::sentinel_t<View>>
     = default;
-    constexpr explicit sentinel(std::ranges::sentinel_t<View> end) : end_(std::move(end)) {}
+    constexpr explicit sentinel(std::ranges::sentinel_t<View> end)
+      : end_(std::move(end)) {}
 
-    friend constexpr bool operator==(const iterator& x, const sentinel& y) requires
-      std::sentinel_for<std::ranges::sentinel_t<View>, std::ranges::iterator_t<View>> {
+    friend constexpr bool                                     //
+    operator==(const iterator& x, const sentinel& y) requires //
+      std::sentinel_for<std::ranges::sentinel_t<View>,
+                        std::ranges::iterator_t<View>> {
       return x.base() == y.end_;
     }
   };
