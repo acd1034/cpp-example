00d240d31d535aa820fd7ee3e5490d95795346ac
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-19 12:08:48 +0900

    const-iterableに対応

--- include/ns/enumerate_view.hpp
+++ include/ns/enumerate_view.hpp
@@ -14,7 +14,9 @@ namespace ns {
     //! 元となる view
     View base_ = View();
 
+    template <bool Const>
     struct iterator;
+    template <bool Const>
     struct sentinel;
 
   public:
@@ -22,17 +24,33 @@ namespace ns {
     = default;
     constexpr enumerate_view(View base) : base_(std::move(base)) {}
 
-    constexpr iterator begin() { return {std::ranges::begin(base_), 0}; }
+    constexpr iterator<false> begin() { return {std::ranges::begin(base_), 0}; }
+    constexpr iterator<true>
+    begin() const requires std::ranges::input_range<const View> {
+      return {std::ranges::begin(base_), 0};
+    }
 
     constexpr auto end() {
       if constexpr (std::ranges::common_range<View> and //
                     std::ranges::sized_range<View>)
-        return iterator(std::ranges::end(base_), std::ranges::size(base_));
+        return iterator<false>(std::ranges::end(base_),
+                               std::ranges::size(base_));
       else
-        return sentinel(std::ranges::end(base_));
+        return sentinel<false>(std::ranges::end(base_));
+    }
+    constexpr auto end() const requires std::ranges::input_range<const View> {
+      if constexpr (std::ranges::common_range<const View> and //
+                    std::ranges::sized_range<const View>)
+        return iterator<true>(std::ranges::end(base_),
+                              std::ranges::size(base_));
+      else
+        return sentinel<true>(std::ranges::end(base_));
     }
 
     constexpr auto size() requires std::ranges::sized_range<View> {
+      return std::ranges::size(base_);
+    }
+    constexpr auto size() const requires std::ranges::sized_range<const View> {
       return std::ranges::size(base_);
     }
   };
@@ -54,38 +72,44 @@ namespace ns {
 
   template <std::ranges::input_range View>
   requires std::ranges::view<View>
+  template <bool Const>
   struct enumerate_view<View>::iterator : deduce_iterator_category<View> {
   private:
+    using Base = std::conditional_t<Const, const View, View>;
     //! 元となるイテレータの現在位置
-    std::ranges::iterator_t<View> current_ = std::ranges::iterator_t<View>();
+    std::ranges::iterator_t<Base> current_ = std::ranges::iterator_t<Base>();
     //! 現在のインデックス
     std::size_t count_ = 0;
 
   public:
-    using difference_type = std::ranges::range_difference_t<View>;
-    using value_type = std::pair<std::size_t, std::ranges::range_value_t<View>>;
+    using difference_type = std::ranges::range_difference_t<Base>;
+    using value_type = std::pair<std::size_t, std::ranges::range_value_t<Base>>;
     // clang-format off
     using iterator_concept =
-      std::conditional_t<std::ranges::random_access_range<View>, std::random_access_iterator_tag,
-      std::conditional_t<std::ranges::bidirectional_range<View>, std::bidirectional_iterator_tag,
-      std::conditional_t<std::ranges::forward_range<View>,       std::forward_iterator_tag,
+      std::conditional_t<std::ranges::random_access_range<Base>, std::random_access_iterator_tag,
+      std::conditional_t<std::ranges::bidirectional_range<Base>, std::bidirectional_iterator_tag,
+      std::conditional_t<std::ranges::forward_range<Base>,       std::forward_iterator_tag,
       /* else */                                                 std::input_iterator_tag>>>;
     // clang-format on
 
     iterator() requires
-      std::default_initializable<std::ranges::iterator_t<View>>
+      std::default_initializable<std::ranges::iterator_t<Base>>
     = default;
-    constexpr iterator(std::ranges::iterator_t<View> current, std::size_t count)
+    constexpr iterator(std::ranges::iterator_t<Base> current, std::size_t count)
       : current_(std::move(current)), count_(std::move(count)) {}
+    constexpr /* implicit */ iterator(iterator<not Const> other) requires Const
+      and std::convertible_to<std::ranges::iterator_t<View>,
+                              std::ranges::iterator_t<Base>>
+      : current_(std::move(other.current_)), count_(std::move(other.count_)) {}
 
-    constexpr const std::ranges::iterator_t<View>& base() const& noexcept {
+    constexpr const std::ranges::iterator_t<Base>& base() const& noexcept {
       return current_;
     }
-    constexpr std::ranges::iterator_t<View> base() && {
+    constexpr std::ranges::iterator_t<Base> base() && {
       return std::move(current_);
     }
 
-    constexpr std::pair<std::size_t, std::ranges::range_reference_t<View>> //
+    constexpr std::pair<std::size_t, std::ranges::range_reference_t<Base>>
     operator*() const {
       return {count_, *current_};
     }
@@ -97,86 +121,86 @@ namespace ns {
     }
     constexpr void operator++(int) { ++*this; }
     constexpr iterator
-    operator++(int) requires std::ranges::forward_range<View> {
+    operator++(int) requires std::ranges::forward_range<Base> {
       auto tmp = *this;
       ++*this;
       return tmp;
     }
 
     constexpr iterator&
-    operator--() requires std::ranges::bidirectional_range<View> {
+    operator--() requires std::ranges::bidirectional_range<Base> {
       --current_;
       --count_;
       return *this;
     }
     constexpr iterator
-    operator--(int) requires std::ranges::bidirectional_range<View> {
+    operator--(int) requires std::ranges::bidirectional_range<Base> {
       auto tmp = *this;
       --*this;
       return tmp;
     }
 
     constexpr iterator& operator+=(difference_type n) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       current_ += n;
       count_ += n;
       return *this;
     }
     constexpr iterator& operator-=(difference_type n) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return *this += -n;
     }
-    constexpr std::pair<std::size_t, std::ranges::range_reference_t<View>>
+    constexpr std::pair<std::size_t, std::ranges::range_reference_t<Base>>
     operator[](difference_type n) const //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return *(*this + n);
     }
 
     friend constexpr bool operator==(const iterator& x, const iterator& y) //
-      requires std::equality_comparable<std::ranges::iterator_t<View>> {
+      requires std::equality_comparable<std::ranges::iterator_t<Base>> {
       return x.current_ == y.current_;
     }
 
     friend constexpr bool operator<(const iterator& x, const iterator& y) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return x.current_ < y.current_;
     }
     friend constexpr bool operator>(const iterator& x, const iterator& y) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return y < x;
     }
     friend constexpr bool operator<=(const iterator& x, const iterator& y) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return not(y < x);
     }
     friend constexpr bool operator>=(const iterator& x, const iterator& y) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       return not(x < y);
     }
     friend constexpr auto operator<=>(const iterator& x, const iterator& y) //
-      requires std::ranges::random_access_range<View> and                   //
-      std::three_way_comparable<std::ranges::iterator_t<View>> {
+      requires std::ranges::random_access_range<Base> and                   //
+      std::three_way_comparable<std::ranges::iterator_t<Base>> {
       return x.current_ <=> y.current_;
     }
 
     friend constexpr iterator operator+(iterator x, difference_type n) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       x += n;
       return x;
     }
     friend constexpr iterator operator+(difference_type n, iterator x) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       x += n;
       return x;
     }
     friend constexpr iterator operator-(iterator x, difference_type n) //
-      requires std::ranges::random_access_range<View> {
+      requires std::ranges::random_access_range<Base> {
       x -= n;
       return x;
     }
     friend constexpr difference_type //
     operator-(const iterator& x, const iterator& y) requires
-      std::ranges::random_access_range<View> {
+      std::ranges::random_access_range<Base> {
       return x.current_ - y.current_;
     }
 
@@ -190,33 +214,39 @@ namespace ns {
 
   template <std::ranges::input_range View>
   requires std::ranges::view<View>
+  template <bool Const>
   struct enumerate_view<View>::sentinel {
   private:
+    using Base = std::conditional_t<Const, const View, View>;
     //! 元となる view の番兵イテレータ
-    std::ranges::sentinel_t<View> end_ = std::ranges::sentinel_t<View>();
+    std::ranges::sentinel_t<Base> end_ = std::ranges::sentinel_t<Base>();
 
   public:
     sentinel() = default;
-    constexpr explicit sentinel(std::ranges::sentinel_t<View> end)
+    constexpr explicit sentinel(std::ranges::sentinel_t<Base> end)
       : end_(std::move(end)) {}
+    constexpr /* implicit */ sentinel(sentinel<not Const> other) requires Const
+      and std::convertible_to<std::ranges::sentinel_t<View>,
+                              std::ranges::sentinel_t<Base>>
+      : end_(std::move(other.end_)) {}
 
-    friend constexpr bool                                     //
-    operator==(const iterator& x, const sentinel& y) requires //
-      std::sentinel_for<std::ranges::sentinel_t<View>,
-                        std::ranges::iterator_t<View>> {
+    friend constexpr bool //
+    operator==(const iterator<Const>& x, const sentinel& y) requires
+      std::sentinel_for<std::ranges::sentinel_t<Base>,
+                        std::ranges::iterator_t<Base>> {
       return x.base() == y.end_;
     }
 
-    friend constexpr std::ranges::range_difference_t<View> //
-    operator-(const iterator& x, const sentinel& y) requires
-      std::sized_sentinel_for<std::ranges::sentinel_t<View>,
-                              std::ranges::iterator_t<View>> {
+    friend constexpr std::ranges::range_difference_t<Base>
+    operator-(const iterator<Const>& x, const sentinel& y) requires
+      std::sized_sentinel_for<std::ranges::sentinel_t<Base>,
+                              std::ranges::iterator_t<Base>> {
       return x.base() - y.end_;
     }
-    friend constexpr std::ranges::range_difference_t<View>
-    operator-(const sentinel& x, const iterator& y) requires
-      std::sized_sentinel_for<std::ranges::sentinel_t<View>,
-                              std::ranges::iterator_t<View>> {
+    friend constexpr std::ranges::range_difference_t<Base>
+    operator-(const sentinel& x, const iterator<Const>& y) requires
+      std::sized_sentinel_for<std::ranges::sentinel_t<Base>,
+                              std::ranges::iterator_t<Base>> {
       return x.end_ - y.base();
     }
   };
--- tests/enumerate_view/enumerate_view.cpp
+++ tests/enumerate_view/enumerate_view.cpp
@@ -219,3 +219,169 @@ TEST_CASE("enumerate_view", "[enumerate_view][common]") {
     CHECK(it == std::ranges::end(ev));
   }
 }
+
+TEST_CASE("enumerate_view", "[enumerate_view][const]") {
+  {
+    test_range<char> tr{'a'};
+    const ns::enumerate_view ev(tr);
+    static_assert(std::ranges::input_range<decltype(ev)>);
+    static_assert(not std::ranges::forward_range<decltype(ev)>);
+    static_assert(not std::ranges::common_range<decltype(ev)>);
+
+    std::input_iterator auto it = std::ranges::begin(ev);
+    // test_cpp20_input_iterator は _Cpp17InputIterator_ 要件を満たさない
+    // static_assert(
+    //   not std::derived_from<
+    //     typename std::iterator_traits<decltype(it)>::iterator_category,
+    //     std::input_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it) == 'a');
+    ++it;
+    CHECK(it == std::ranges::end(ev));
+  }
+  {
+    std::forward_list<char> fl{'a', 'b', 'c'};
+    const ns::enumerate_view ev(fl);
+    static_assert(std::ranges::forward_range<decltype(ev)>);
+    static_assert(not std::ranges::bidirectional_range<decltype(ev)>);
+    static_assert(not std::ranges::common_range<decltype(ev)>);
+    static_assert(std::same_as<std::ranges::range_reference_t<decltype(ev)>,
+                               std::pair<unsigned long, char&>>);
+    static_assert(std::same_as<std::ranges::range_value_t<decltype(ev)>,
+                               std::pair<unsigned long, char>>);
+
+    std::forward_iterator auto it = std::ranges::begin(ev);
+    static_assert(
+      std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::input_iterator_tag>);
+    static_assert(
+      not std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::forward_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it++) == 'a');
+    CHECK(std::get<0>(*it) == 1);
+    CHECK(std::get<1>(*it++) == 'b');
+    CHECK(std::get<0>(*it) == 2);
+    CHECK(std::get<1>(*it++) == 'c');
+    CHECK(it == std::ranges::end(ev));
+  }
+  {
+    std::list<char> l{'a', 'b', 'c'};
+    const ns::enumerate_view ev(l);
+    static_assert(std::ranges::bidirectional_range<decltype(ev)>);
+    static_assert(not std::ranges::random_access_range<decltype(ev)>);
+    static_assert(std::ranges::common_range<decltype(ev)>);
+
+    std::bidirectional_iterator auto it = std::ranges::begin(ev);
+    static_assert(
+      std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::input_iterator_tag>);
+    static_assert(
+      not std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::forward_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it++) == 'a');
+    CHECK(std::get<0>(*it) == 1);
+    CHECK(std::get<1>(*it++) == 'b');
+    CHECK(std::get<0>(*it) == 2);
+    CHECK(std::get<1>(*it++) == 'c');
+    CHECK(it == std::ranges::end(ev));
+  }
+  {
+    std::vector<char> v{'a', 'b', 'c'};
+    const ns::enumerate_view ev(v);
+    static_assert(std::ranges::random_access_range<decltype(ev)>);
+    static_assert(std::ranges::common_range<decltype(ev)>);
+
+    std::random_access_iterator auto it = std::ranges::begin(ev);
+    static_assert(
+      std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::input_iterator_tag>);
+    static_assert(
+      not std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::forward_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it++) == 'a');
+    CHECK(std::get<0>(*it) == 1);
+    CHECK(std::get<1>(*it++) == 'b');
+    CHECK(std::get<0>(*it) == 2);
+    CHECK(std::get<1>(*it++) == 'c');
+    CHECK(it == std::ranges::end(ev));
+  }
+  {
+    std::vector<char> v{'a', 'b', 'c'};
+    const ns::enumerate_view ev(v);
+    std::random_access_iterator auto begin = std::ranges::begin(ev);
+    auto end = std::ranges::end(ev);
+    static_assert(std::sized_sentinel_for<decltype(end), decltype(begin)>);
+    CHECK(std::ranges::end(ev) - std::ranges::begin(ev) == 3);
+  }
+  {
+    std::vector<char> v{'a', 'b', 'c'};
+    const ns::enumerate_view ev(v);
+    static_assert(std::ranges::sized_range<decltype(ev)>);
+    CHECK(std::ranges::size(ev) == 3);
+  }
+}
+
+TEST_CASE("enumerate_view", "[enumerate_view][const][common]") {
+  {
+    std::list<char> l{'a', 'b', 'c'};
+    std::ranges::subrange sr(std::counted_iterator(std::ranges::begin(l), 2),
+                             std::default_sentinel);
+    const ns::enumerate_view ev(sr);
+    static_assert(std::ranges::bidirectional_range<decltype(ev)>);
+    static_assert(not std::ranges::random_access_range<decltype(ev)>);
+    static_assert(not std::ranges::common_range<decltype(ev)>);
+
+    std::bidirectional_iterator auto it = std::ranges::begin(ev);
+    static_assert(
+      std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::input_iterator_tag>);
+    static_assert(
+      not std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::forward_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it++) == 'a');
+    CHECK(std::get<0>(*it) == 1);
+    CHECK(std::get<1>(*it++) == 'b');
+    CHECK(it == std::ranges::end(ev));
+  }
+  {
+    std::vector<char> v{'a', 'b', 'c'};
+    std::ranges::subrange sr(std::counted_iterator(std::ranges::begin(v), 2),
+                             std::default_sentinel);
+    const ns::enumerate_view ev(sr);
+    static_assert(std::ranges::random_access_range<decltype(ev)>);
+    static_assert(not std::ranges::common_range<decltype(ev)>);
+
+    std::random_access_iterator auto it = std::ranges::begin(ev);
+    static_assert(
+      std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::input_iterator_tag>);
+    static_assert(
+      not std::derived_from<
+        typename std::iterator_traits<decltype(it)>::iterator_category,
+        std::forward_iterator_tag>);
+
+    CHECK(std::get<0>(*it) == 0);
+    CHECK(std::get<1>(*it++) == 'a');
+    CHECK(std::get<0>(*it) == 1);
+    CHECK(std::get<1>(*it++) == 'b');
+    CHECK(it == std::ranges::end(ev));
+  }
+}
